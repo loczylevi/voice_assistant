96f0cd4872d658c4d246bcfa92bdc8053c313613
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-12-06 22:27:51 +0100

    Update hun_assistan.py

--- hun_assistan.py
+++ hun_assistan.py
@@ -6,7 +6,7 @@ import pywhatkit as kit
 time_now = datetime.now()
 current_time = time_now.strftime("%H:%M")
 
-kit.playonyt("Highway to hell")
+#kit.playonyt("Highway to hell")
 
 ido = str(current_time)
 ido_szak = ""
@@ -16,12 +16,25 @@ elif "10" <= ido[:1] <= "16":
                     ido_szak = "jónapot"
 else:
                     ido_szak = "jóestét"
+figy = ""
+if ido[:1] >= "10":
+                    figy = "Ideje lefeküdni"
+
 text = f"{ido_szak} Levente {current_time} van"
 lang = "hu"
 speech = Speech(text, lang)
 speech.play()
+if len(figy) > 0:
+                    time.sleep(0.5)
+                    text = f"{figy}"
+                    lang = "hu"
+                    speech = Speech(text, lang)
+                    speech.play()
 time.sleep(0.5)
-text = "Jó kódolást kivánok"
-lang = "hu"
-speech = Speech(text, lang)
-speech.play()
+if len(figy) > 0:
+                    pass
+else:
+                    text = "Jó kódolást kivánok"
+                    lang = "hu"
+                    speech = Speech(text, lang)
+                    speech.play()
